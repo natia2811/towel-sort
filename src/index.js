c8abde1aed706305a98f35c96455611062104479
natia2811 <kor-natiya@yandex.ru>
2021-03-03 23:12:42 +0400

    feat: add function to create dream team name

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let tempArr=[];
+if(!matrix){
+  return tempArr;
+}
+if(!matrix.length){
+  return tempArr;
+}
+for (i=0; i<=matrix.length; i++){
+  let isOdd = i%2 !==0;
+
+  if(isOdd){
+   tempArr = tempArr.concat(matrix[i]);
+  }
+
+  else{
+    let reverseArr = matrix[i].toString().split('').reverse().join('');
+    tempArr = tempArr.concat(reverseArr);
+  }
+}
+
+return tempArr;
 }
